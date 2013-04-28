56fabc8298ac0e6cee877f31351d689b40cfc379
Gareth Davis <gareth.davis@mac.com>
2013-04-28 17:11:48 +0100

    initial project files

---
+++ src/test/groovy/toy/ToyTasksTest.groovy
@@ -0,0 +1,36 @@
+package toy
+
+import groovy.util.logging.Slf4j
+import org.gradle.api.Project
+import org.gradle.testfixtures.ProjectBuilder
+import org.junit.Test
+
+/**
+ *
+ */
+class ToyTasksTest {
+
+    boolean task1Run = false
+    boolean task2Run = false
+
+    @Test
+    public void taskDependencies(){
+
+
+        Project p = ProjectBuilder.builder().build()
+
+        p.task("task1") << {
+            p.logger.info("task1 running")
+            task1Run = true
+        }
+
+        def task2 = p.task("task2", dependsOn: 'task1') << {
+            p.logger.info("task2 running")
+            task2Run = true
+        }
+        task2.execute() // <--- what magjc do I need here instead of .execute()
+
+        assert task2Run == true
+        assert task1Run == true
+    }
+}
